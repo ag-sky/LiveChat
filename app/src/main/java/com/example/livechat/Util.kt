296f22a39830b673812236612e5186addb386bcb
Akash Gupta <ag.office00@gmail.com>
2024-01-26 19:36:29 +0530

    ref #4 ui of sign up screen done

---
+++ app/src/main/java/com/example/livechat/Util.kt
@@ -0,0 +1,10 @@
+package com.example.livechat
+
+import androidx.navigation.NavController
+
+fun navigateTo(navController: NavController, route: String){
+    navController.navigate(route){
+        popUpTo(route)
+        launchSingleTop = true
+    }
+}
