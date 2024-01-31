34f7c79d2ce9bd21c95722ff034efa475e06aa0c
Akash Gupta <ag.office00@gmail.com>
2024-02-01 00:39:31 +0530

    ref #8 chat list screen done

--- app/src/main/java/com/example/livechat/Screens/ChatListScreen.kt
+++ app/src/main/java/com/example/livechat/Screens/ChatListScreen.kt
@@ -1,13 +1,152 @@
 package com.example.livechat.Screens
 
+import android.icu.text.CaseMap.Title
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.livechat.CommonRow
+import com.example.livechat.DestinationScreen
 import com.example.livechat.LCViewModel
+import com.example.livechat.TitleText
+import com.example.livechat.commonProgressBar
+import com.example.livechat.navigateTo
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ChatListScreen(navController: NavController, vm: LCViewModel) {
-    
-    Text(text = "Chat list")
-    BottomNavigationMenu(selectedItem =BottomNavigationItem.CHATLIST , navController = navController)
+    val inProgress = vm.inProgressChats
+    if (inProgress.value)
+        commonProgressBar()
+    else {
+        val chats = vm.chats.value
+        val userData = vm.userData.value
+        val showDialog = remember {
+            mutableStateOf(false)
+        }
+        val onFabClick: () -> Unit = { showDialog.value = true }
+        val onDismissClick: () -> Unit = { showDialog.value = false }
+        val onAddChat: (String) -> Unit = {
+            vm.onAddChat(it)
+            showDialog.value = false
+        }
+
+        Scaffold(
+            floatingActionButton = {
+                FAB(
+                    showDialog = showDialog.value,
+                    onFabClick = onFabClick,
+                    onDismiss = onDismissClick,
+                    onAddChat = onAddChat
+                )
+            },
+            content = {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(it)
+                ) {
+                    TitleText(text = "Chats")
+                    if (chats.isEmpty()) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(1f),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Text(text = "No Chats Available")
+                        }
+                    } else {
+                        LazyColumn(modifier = Modifier.weight(1f)) {
+                            items(chats) { chat ->
+                                val chatUser = if (chat.user1.userId == userData?.userId) {
+                                    chat.user2
+                                } else {
+                                    chat.user1
+                                }
+                                CommonRow(imageUrl = chatUser.imageUrl, name = chatUser.name) {
+                                    chat.chatId?.let {
+                                        navigateTo(
+                                            navController = navController,
+                                            route = DestinationScreen.SingleChat.createRoute(id = it)
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    BottomNavigationMenu(
+                        selectedItem = BottomNavigationItem.CHATLIST,
+                        navController = navController
+                    )
+                }
+            }
+        )
+    }
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FAB(
+    showDialog: Boolean,
+    onFabClick: () -> Unit,
+    onDismiss: () -> Unit,
+    onAddChat: (String) -> Unit
+) {
+
+    val addChatNumber = remember {
+        mutableStateOf("")
+    }
+
+    if (showDialog) {
+        AlertDialog(onDismissRequest = {
+            onDismiss.invoke()
+            addChatNumber.value = ""
+        }, confirmButton = {
+            Button(onClick = { onAddChat(addChatNumber.value) }) {
+                Text(text = "Add Chat")
+            }
+        }, title = { Text(text = "Add Chat") }, text = {
+            OutlinedTextField(
+                value = addChatNumber.value,
+                onValueChange = { addChatNumber.value = it },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+        })
+        FloatingActionButton(
+            onClick = { onFabClick },
+            containerColor = MaterialTheme.colorScheme.secondary,
+            shape = CircleShape,
+            modifier = Modifier.padding(bottom = 40.dp)
+        ) {
+            Icon(imageVector = Icons.Rounded.Add, contentDescription = null, tint = Color.White)
+        }
+    }
 }
--- app/src/main/java/com/example/livechat/Util.kt
+++ app/src/main/java/com/example/livechat/Util.kt
@@ -6,19 +6,27 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import coil.compose.rememberImagePainter
 
@@ -49,28 +57,62 @@ fun commonProgressBar() {
 
 @Composable
 fun checkSignedIn(vm: LCViewModel, navController: NavController) {
-  val alreadySignedIn = remember {
-      mutableStateOf(false)
-  }
-      val signIn = vm.signIn.value
-      if(signIn && !alreadySignedIn.value){
-          alreadySignedIn.value = true
-        navController.navigate(DestinationScreen.ChatList.route){
+    val alreadySignedIn = remember {
+        mutableStateOf(false)
+    }
+    val signIn = vm.signIn.value
+    if (signIn && !alreadySignedIn.value) {
+        alreadySignedIn.value = true
+        navController.navigate(DestinationScreen.ChatList.route) {
             popUpTo(0)
         }
     }
 }
 
 @Composable
-fun CommonImage(data : String?, modifier: Modifier = Modifier.wrapContentSize(), contentScale : ContentScale = ContentScale.Crop){
-    val painter = rememberImagePainter(data =data )
-    Image(painter = painter , contentDescription = null, modifier = modifier, contentScale = contentScale)
+fun CommonImage(
+    data: String?,
+    modifier: Modifier = Modifier.wrapContentSize(),
+    contentScale: ContentScale = ContentScale.Crop
+) {
+    val painter = rememberImagePainter(data = data)
+    Image(
+        painter = painter,
+        contentDescription = null,
+        modifier = modifier,
+        contentScale = contentScale
+    )
 
 }
 
 @Composable
-fun commmonDivider(){
-    Divider(color = Color.LightGray, thickness = 1.dp, modifier = Modifier
-        .alpha(0.3f)
-        .padding(top = 8.dp, bottom = 8.dp))
+fun commmonDivider() {
+    Divider(
+        color = Color.LightGray, thickness = 1.dp, modifier = Modifier
+            .alpha(0.3f)
+            .padding(top = 8.dp, bottom = 8.dp)
+    )
+}
+
+@Composable
+fun TitleText(text: String) {
+    Text(
+        text = text,
+        fontWeight = FontWeight.Bold,
+        fontSize = 35.sp,
+        modifier = Modifier.padding(8.dp)
+    )
+}
+
+@Composable
+fun CommonRow(imageUrl : String?, name : String?, onItemClick:() -> Unit){
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .height(75.dp)
+        .clickable {
+            onItemClick.invoke()
+        }, verticalAlignment = Alignment.CenterVertically){
+        CommonImage(data = imageUrl, modifier = Modifier.padding(8.dp).size(50.dp).clip(CircleShape).background(Color.Red))
+        Text(text = name?: ".....", fontWeight = FontWeight.Bold, modifier = Modifier.padding(start = 4.dp))
+    }
 }
